b8844f461c397680c24e38484a1d6d404dd04ee7
nanel96 <nanel96@yahoo.com>
2021-07-15 13:31:05 -0600

    Update align.sh

--- align.sh
+++ align.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=168:00:00   # walltime
 #SBATCH --ntasks=2   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --mem-per-cpu=8192M   # memory per CPU core
+#SBATCH --mem-per-cpu=51200M   # memory per CPU core
 #SBATCH -J "lambda digestion alignment"   # job name
 #SBATCH --mail-user=nanelbarton@gmail.com   # email address
 #SBATCH --mail-type=BEGIN
